d6dad729e3fbddc07394db31348b6a4283326918
Ehab Reda <ehabred04@gmail.com>
2021-02-27 15:01:25 +0200

    footer

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
      return (
           <footer className="bg-light">
-               <p>{new Date().getFullYear()} ©  Crafted with ❤️  by Mohammed Reda</p>
+               <p>{new Date().getFullYear()} ©  Created with ❤️  by Mohammed Reda</p>
           </footer>
      )
 }
